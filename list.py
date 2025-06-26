f32ba12f278a46dec2de13b25aaaf41f245ab5dc
tabrin15 <tabrinanan01@gmail.com>
2025-06-26 21:10:53 +0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,34 @@
+empty_list =[]
+print(empty_list)
+
+print()
+print()
+
+
+numbers = [1, 2, 3, 4,5]
+print(numbers)
+
+print()
+print()
+
+triples = [1, 2, 3]*3
+print(triples)
+print()
+print()
+
+
+alist = [100, 200, 300, 400, 500]
+alist = alist[::-1]
+print(alist, "\n")
+
+print()
+print()
+
+print(numbers[3])
+print()
+print()
+
+print(numbers[1:3])
+print()
+print()
+
