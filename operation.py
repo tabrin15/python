76c59cfb45deae5877f59768c7e5ffa57559ca2f
tabrin15 <tabrinanan01@gmail.com>
2025-07-04 20:15:31 +0600

    Add files via upload

---
+++ operation.py
@@ -0,0 +1,22 @@
+my_set = {1, 2, 3}
+print(my_set)
+print()
+
+my_set = {1.0, "Hello", (1, 2, 3)}
+print(my_set)
+
+my_set= {1, 2, 3, 4, 5, 5, 4, 3, 6}
+print()
+
+
+my_set = set([1, 2, 3, 3, 2])
+print(my_set,"\n")
+
+num_set = set([0, 1, 2, 3, 4, 5 ])
+print("original set:")
+print(num_set)
+print()
+      
+num_set.pop()
+print("After removing the first element from the said set:")
+print(num_set, "\n")
---
+++ setx.py
@@ -0,0 +1,11 @@
+setx ={"green", "blue"}
+sety = {"blue", "yellow"}
+print("original set elements:")
+
+print(setx)
+print(sety)
+print()
+
+print("\nInteraction of two said sets:")
+setz = setx.intersection(sety)
+print(setz)
