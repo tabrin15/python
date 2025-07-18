99fa614ba722bedd9ec18a26ebde5e116270cd0e
tabrin15 <tabrinanan01@gmail.com>
2025-07-18 19:57:10 +0600

    Add files via upload

---
+++ vehicle.py
@@ -0,0 +1,20 @@
+class Vehicle:
+    def __init__(self, name, color):
+
+        self.color = color
+        self.name = name
+
+
+
+
+Vehiclelamborghini = Vehicle('Lamborghini', 'green')
+VehicleFerrari = Vehicle('Ferrari', 'red')
+
+
+
+print("Vehiclelamborghini name : ", Vehiclelamborghini.name)
+print("Vehiclelamborghini color", Vehiclelamborghini.color)
+
+
+
+
